c32a4e3068f84764e19113e9aaa473126a389faa
NgJinHanJoshua <152830107+NgJinHanJoshua@users.noreply.github.com>
2025-10-15 20:25:10 +0800

    Update NotificationService.java

--- clinicsystem/src/main/java/aqms/service/NotificationService.java
+++ clinicsystem/src/main/java/aqms/service/NotificationService.java
@@ -17,11 +17,12 @@ package aqms.service;
 
 import aqms.domain.model.Patient;
 
+// NOTE: The NotificationService class is written by Ng Jin Han, Joshua.
 public class NotificationService {
     // send da notification to da respective patient
     public void notifyPatient(Patient patient, String message) {
         // System.out.print(patient.getFullName());
-        System.out.println("## A notification has been sent to " + patient.getFullName() + ": " + message + " ##");
+        System.out.println("Notification Sent. Recipient is: " + patient.getFullName() + ": " + message + ".");
     }
 
 }
