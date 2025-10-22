669987fee5e0dc7a25ed234d3d0218b067afea4d
NgJinHanJoshua <152830107+NgJinHanJoshua@users.noreply.github.com>
2025-10-22 15:24:39 +0800

    Update NotificationService.java

--- clinicsystem/src/main/java/aqms/service/NotificationService.java
+++ clinicsystem/src/main/java/aqms/service/NotificationService.java
@@ -1,7 +1,8 @@
 package aqms.service;
-
 import aqms.domain.model.Patient;
+import org.springframework.stereotype.Service;
 
+@Service
 // NOTE: The NotificationService class is written by Ng Jin Han, Joshua.
 public class NotificationService {
     // send da notification to da respective patient
