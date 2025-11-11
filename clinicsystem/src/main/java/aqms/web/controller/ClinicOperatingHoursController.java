ab5933f353dd7b8b4569ce180c15664eb002c382
youfarny <faith.ong.2023@scis.smu.edu.sg>
2025-11-11 18:58:41 +0800

    minor update

--- clinicsystem/src/main/java/aqms/web/controller/ClinicOperatingHoursController.java
+++ clinicsystem/src/main/java/aqms/web/controller/ClinicOperatingHoursController.java
@@ -12,13 +12,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/clinic-operating-hours")
-@RequiredArgsConstructor
 /**
  * ClinicOperatingHoursController
  *
  * Exposes endpoints to view and modify clinic operating hours and related
  * helper endpoints used by the UI to determine if a clinic is open.
  */
+@RequiredArgsConstructor
 public class ClinicOperatingHoursController {
     private final ClinicOperatingHoursService hoursService;
 
